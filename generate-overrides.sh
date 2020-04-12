c01c7df65a5d4ea238537f34fb1cf66065b9d9c3
Josh Ghiloni <jghiloni@pivotal.io>
2020-04-12 17:04:22 -0600

    More fixes and overrides

--- generate-overrides.sh
+++ generate-overrides.sh
@@ -6,9 +6,11 @@ BASEDIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
 RELEASE=${BASEDIR}/release
 
 logsearch_boshrelease_dir=$1
+logsearch_for_cloudfoundry_dir=$2
 shift
 
 logsearch_boshrelease_dir=$(realpath ${logsearch_boshrelease_dir})
+logsearch_for_cloudfoundry_dir=$(realpath ${logsearch_for_cloudfoundry_dir})
 
 #### COPY THE INGESTOR_SYSLOG JOB ####
 # We want this to be as similar to the standard logsearch ingestor_syslog job 
@@ -39,20 +41,28 @@ cat ${JOB_DIR}/config/override-defaults.conf >> ${JOB_DIR}/config/logstash.conf
 EOF
 tail -n+${line} ${BASEDIR}/ingestor_syslog.tmp >> ${RELEASE}/jobs/ingestor_syslog/templates/bin/ingestor_syslog
 
-### COPY THE LOGSEARCH-CONFIG PACKAGE ####
+### COPY THE KIBANA-AUTH-PLUGIN JOB
+cp -Rvf ${logsearch_for_cloudfoundry_dir}/jobs/kibana-auth-plugin ${RELEASE}/jobs/
+
+### COPY THE LOGSEARCH-CONFIG PACKAGE 
 cp -Rvf ${logsearch_boshrelease_dir}/packages/logsearch-config ${RELEASE}/packages/
 cp -Rvf ${logsearch_boshrelease_dir}/src/logsearch-config ${RELEASE}/src/
 cp -Rvf ${BASEDIR}/overrides/templates/src/logsearch-config/* ${RELEASE}/src/logsearch-config/
 
+### COPY THE KIBANA-AUTH-PLUGIN PACKAGE 
+cp -Rvf ${logsearch_for_cloudfoundry_dir}/packages/kibana-auth-plugin ${RELEASE}/packages/
+cp -Rvf ${logsearch_for_cloudfoundry_dir}/src/kibana-cf_authentication ${RELEASE}/src/
+cp -Rvf ${BASEDIR}/overrides/template/src/kibana-cf_authentication/* ${RELEASE}/src/kibana-cf_authentication/
+
 ### VENDOR ALL OTHER NECESSARY PACKAGES
 pushd ${RELEASE}
-for package in $(bosh int jobs/ingestor_syslog/spec --path /packages | awk '{print $2}' | grep -v logsearch-config); do
-    bosh vendor-package ${package} ${logsearch_boshrelease_dir}
-done
+    for package in $(bosh int jobs/ingestor_syslog/spec --path /packages | awk '{print $2}' | grep -v logsearch-config); do
+        bosh vendor-package ${package} ${logsearch_boshrelease_dir}
+    done
 
-for package in $(bosh int packages/logsearch-config/spec --path /dependencies | awk '{print $2}' | grep -v logsearch-config); do
-    bosh vendor-package ${package} ${logsearch_boshrelease_dir}
-done
+    for package in $(bosh int packages/logsearch-config/spec --path /dependencies | awk '{print $2}' | grep -v logsearch-config); do
+        bosh vendor-package ${package} ${logsearch_boshrelease_dir}
+    done
 popd
 
 ### UPLOAD ALL BLOBS
